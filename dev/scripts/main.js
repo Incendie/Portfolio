cf5bdf31bca67bfe4cc751eb8ddf228d7382ca17
Incendie <incendie323@gmail.com>
2017-09-16 17:06:53 -0400

    first passthrough completion to footer

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,3 +1,14 @@
-const ohHey = "Hello World";
-
-console.log(ohHey);
+$(()=>{
+  $('.main-carousel').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true,
+    accessibility: true,
+    setGallerySize: false,
+    cellSelector: '.carousel-cell',
+    imagesLoaded: true,
+    wrapAround: true,
+    autoPlay: 3000,
+    pauseAutoPlayOnHover: false
+  });
+});
