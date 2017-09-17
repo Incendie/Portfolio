f710121dde8be209d741da6cccff121753a3e3e0
Incendie <incendie323@gmail.com>
2017-09-16 21:19:21 -0400

    contrasting nav bar when it hits the skills and contact section

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,3 +1,38 @@
+const portfolio = {};
+portfolio.contrastNav = () => {
+  let scrollStart = 0;
+  let headerHomeChange = $('#home').offset().top;
+  let headerAboutChange = $('#home').offset().top;
+  let skillChange = $('#skills').offset().top;
+  let contactChange = $('#contact').offset().top;
+
+  $(document).scroll( () => {
+    scrollStart = $(document).scrollTop();    
+    console.log(scrollStart);
+    if ((scrollStart > headerHomeChange+70 && scrollStart < headerHomeChange+450)) {
+      $(".header__nav li:first-child > a").addClass("contrast");
+    }
+    else {
+      $(".header__nav li:first-child > a").removeClass("contrast");
+    }
+
+    if ((scrollStart > skillChange-100 && scrollStart < skillChange+775) || (scrollStart > contactChange-100)) {
+      $(".header__nav li > a").addClass("contrast");
+    }
+    else {
+        $(".header__nav li > a").removeClass("contrast");
+    }
+    
+    
+
+
+    if (scrollStart > skillChange.top-100) {
+      $("li > a").css("color", "white");
+      $(".hamburger > span").css("background-color", "white");
+    }
+  });
+}
+
 $(()=>{
   $('.main-carousel').flickity({
     // options
@@ -17,4 +52,5 @@ $(()=>{
     $('.header__nav > ul').toggleClass('open');
     $("nav").show();
   });
+  portfolio.contrastNav();  
 });
