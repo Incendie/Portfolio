6b0e39f0045bf2d2d9f21bf2d33a4cf05e66406b
Incendie <incendie323@gmail.com>
2017-09-16 23:03:27 -0400

    completed navigation contrast (no media queries)

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -3,37 +3,46 @@ portfolio.contrastNav = () => {
   let scrollStart = 0;
   let headerHomeChange = $('#home').offset().top;
   let headerAboutChange = $('#home').offset().top;
+  let aboutContactChange = $('#about').offset().top;
   let skillChange = $('#skills').offset().top;
   let contactChange = $('#contact').offset().top;
 
   $(document).scroll( () => {
-    scrollStart = $(document).scrollTop();    
-    console.log(scrollStart);
+    scrollStart = $(document).scrollTop();
     if ((scrollStart > headerHomeChange+70 && scrollStart < headerHomeChange+450)) {
       $(".header__nav li:first-child > a").addClass("contrast");
+      if (scrollStart > headerHomeChange+125){        
+        $(".header__nav li:nth-child(2) > a").addClass("contrast");
+      }
     }
-    else {
-      $(".header__nav li:first-child > a").removeClass("contrast");
+    else if (scrollStart > aboutContactChange+125 && scrollStart < aboutContactChange+225){        
+      $(".header__nav li:last-child > a").addClass("contrast");
     }
-
-    if ((scrollStart > skillChange-100 && scrollStart < skillChange+775) || (scrollStart > contactChange-100)) {
+    else if ((scrollStart > skillChange-100 && scrollStart < skillChange+775) || (scrollStart > contactChange-100)) {
       $(".header__nav li > a").addClass("contrast");
+      $(".hamburger span").addClass("contrast");
+
+      if ((scrollStart > contactChange+250 && scrollStart < contactChange+350) || (scrollStart > contactChange+400 && scrollStart < contactChange+500) || (scrollStart > contactChange+550 && scrollStart < contactChange+750)) {
+        $(".header__nav li:nth-child(2) > a").removeClass("contrast");
+        $(".header__nav li:nth-child(3) > a").removeClass("contrast");
+      }
     }
     else {
         $(".header__nav li > a").removeClass("contrast");
-    }
-    
-    
-
-
-    if (scrollStart > skillChange.top-100) {
-      $("li > a").css("color", "white");
-      $(".hamburger > span").css("background-color", "white");
+        $(".hamburger span").removeClass("contrast");
     }
   });
 }
 
-$(()=>{
+portfolio.hamburgerClick = () => {
+  $('.hamburger').click(function(){
+    $(this).toggleClass('open');
+    $('.header__nav > ul').toggleClass('open');
+    $("nav").show();
+  });
+}
+
+portfolio.flickity = () => {
   $('.main-carousel').flickity({
     // options
     cellAlign: 'left',
@@ -46,11 +55,14 @@ $(()=>{
     autoPlay: 3000,
     pauseAutoPlayOnHover: false
   });
+}
 
-  $('.hamburger').click(function(){
-    $(this).toggleClass('open');
-    $('.header__nav > ul').toggleClass('open');
-    $("nav").show();
-  });
-  portfolio.contrastNav();  
+portfolio.init = () => {
+  portfolio.flickity();
+  portfolio.hamburgerClick();
+  portfolio.contrastNav();
+}
+
+$(()=>{
+  portfolio.init();  
 });
