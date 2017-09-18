2250fca53458fb93855c97475c2a098b67b2ec1c
Anson Li <incendie323@gmail.com>
2017-09-18 14:57:13 -0400

    first completed passthrough for copy, fixed contrast javascript for nav bar

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -6,12 +6,15 @@ portfolio.contrastNav = () => {
   let headerAboutChange = $('#home').offset().top;
   let aboutContactChange = $('#about').offset().top;
   let galleryChange = $('#gallery').offset().top;
+  let inputNameChange = $('#formName').offset().top;
+  let inputEmailChange = $('#formEmail').offset().top;
+  let textareaChange = $('textarea').offset().top;
   let skillChange = $('#skills').offset().top;
   let contactChange = $('#contact').offset().top;
+  let footerChange = $('footer').offset().top;
 
   $(document).scroll( () => {
     scrollStart = $(document).scrollTop();
-    console.log(scrollStart)
     if ((scrollStart > headerHomeChange+70 && scrollStart < headerHomeChange+450)) {
       $(".header__nav li:first-child > a").addClass("contrast");
       if (scrollStart > headerHomeChange+125){        
@@ -25,12 +28,26 @@ portfolio.contrastNav = () => {
       $(".header__nav li:last-child > a").addClass("contrast");
     }
     else if ((scrollStart > skillChange-100 && scrollStart < skillChange+775) || (scrollStart > contactChange-100)) {
-      $(".header__nav li > a").addClass("contrast");
-      $(".hamburger span").addClass("contrast");
-
-      if ((scrollStart > contactChange+250 && scrollStart < contactChange+350) || (scrollStart > contactChange+400 && scrollStart < contactChange+475) || (scrollStart > contactChange+550 && scrollStart < contactChange+750)) {
+      
+      if (scrollStart > inputNameChange-100 && scrollStart < inputNameChange-30){
         $(".header__nav li:nth-child(2) > a").removeClass("contrast");
         $(".header__nav li:nth-child(3) > a").removeClass("contrast");
+      }
+      else if (scrollStart > inputEmailChange-100 && scrollStart < inputEmailChange-30){
+        $(".header__nav li:nth-child(2) > a").removeClass("contrast");
+        $(".header__nav li:nth-child(3) > a").removeClass("contrast");
+      }
+      else if (scrollStart > textareaChange-100 && scrollStart < textareaChange+125){
+        $(".header__nav li:nth-child(2) > a").removeClass("contrast");
+        $(".header__nav li:nth-child(3) > a").removeClass("contrast");
+      }
+      else if (scrollStart > footerChange-100){
+        $(".header__nav li > a").removeClass("contrast");
+        $(".hamburger span").removeClass("contrast");
+      }
+      else {
+        $(".header__nav li > a").addClass("contrast");
+        $(".hamburger span").addClass("contrast");
       }
     }
     else {
@@ -54,11 +71,13 @@ portfolio.flickity = () => {
     cellAlign: 'left',
     contain: true,
     accessibility: true,
-    setGallerySize: false,
+    setGallerySize: true,
+    cellAlign: 'center',
     cellSelector: '.carousel-cell',
     imagesLoaded: true,
     wrapAround: true,
     autoPlay: 3000,
+    
     pauseAutoPlayOnHover: false
   });
 }
